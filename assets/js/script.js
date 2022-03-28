bda8712ccf313bc20613273e960d261ab58cf9e5
Will Tokunaga <wktokunaga@gmail.com>
2022-03-28 12:21:50 -0700

    set up timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
 console.log(`First line`);
+var startBtnEl = document.querySelector(`.start-btn`);
+var timerEl = document.querySelector(`.timer`);
+var score = 0;
+var time = 10; //How long each quiz is
+var timeElapsed = 0;
+
+var decrementTimer = function () {
+    console.log(time - timeElapsed);
+    timeElapsed++;
+    if (timeElapsed === time) {
+        console.log("Time over!");
+        clearInterval()
+    }
+}
+
+// function that starts the quiz, sets counters to 0, starts the timer
+var startQuiz = function () {
+    score = 0;
+    time = 10;
+
+    var timerIntervalID = setInterval(() => {
+        console.log(time - timeElapsed);
+        timeElapsed++;
+        timerEl.textContent = `Time: ${time - timeElapsed}`;
+        if (timeElapsed === time) {
+            console.log("Time over!");
+            clearInterval(timerIntervalID);
+        }
+    }, 1000);
+    //show new question
+}
+
+startBtnEl.addEventListener(`click`, startQuiz);
